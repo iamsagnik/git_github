4818f52e830bdf4dfb62b43c0f0eb18227f23da7
iamsagnik <sagnikmitra008@gmail.com>
2022-09-16 11:01:31 +0530

    Project started

---
+++ main.py
@@ -0,0 +1,14 @@
+msg = """
+Welcome to the program !
+Here you can do following things
+"""
+
+print(msg)
+
+from module1 import add
+
+l = [5, 55, 75, 85, 0, 13, 111, 11, 21]
+
+result = add(l)
+
+print(result)
---
+++ module1.py
@@ -0,0 +1,8 @@
+# calculation module
+
+def add(numbers):
+    total = 0
+    for n in numbers:
+        total = total + n
+
+    return total
